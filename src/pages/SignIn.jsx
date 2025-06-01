f061c29eb38cfd8326af184cff46f57c64f636f6
Shivang Kumar <2002shiva.2002@gmail.com>
2025-06-02 00:51:05 +0530

    SignUp page structure and basic validation done!

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -2,31 +2,66 @@ import React,{useState} from "react";
 const SignIn = () =>{
     const [email,setEmail]=useState("");
     const [password,setPassword]= useState("");
+    const [error,setError]= useState({email: false, password: false});
+    const [success, setSuccess]=useState(false);
+    
 
-    const handleSignIn = () => {
-        alert(`Email:${email}\nPassword: ${password}`)
+    const handleSignIn = (event) => {
+
+
+        event.preventDefault()
+
+        const newError = {
+            email: email.trim() === "",
+            password: password.trim() === "",
+        };
+        setError(newError);
+        if(newError.email||newError.password){
+            //Error exists so need to alert
+            return;
+        }
+       
+        setSuccess(true);
+        setTimeout(() => {
+            setSuccess(false);
+            setEmail("");
+            setPassword("");
+            setError({email:false, password: false });
+        }, 2000);
     };
 
+
     return (
-        <div style={styles.container}>
-            <div styles={styles.heading}>Sign In</div>
+        <form style={styles.container} onSubmit={handleSignIn}>
+            <div style={styles.heading}>Sign In</div>
+            {success && (<div style={{color:"green", fontSize:"14px"}}>Login Successfull!</div>)}
             <input
-            styles={styles.input}
+            style={{...styles.input,
+                border: error.email ? "2px solid red" : "1px solid #ccc",
+            }}
                 type="email"
                 placeholder="Email"
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}>
-            </input>
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            {error.email && <div style={{color:'red' , fontSize:'12px'}}>Email is required !</div>}
 
-            <input style={styles.input} 
+            <input style={{...styles.input,
+                border: error.password ? "2px solid red" : "1px solid #ccc",
+            }} 
             type="password" 
             placeholder="password"  
             value={password} 
             onChange={(e) => setPassword(e.target.value)} 
             />
+            {error.password && <div style={{color:'red' , fontSize:'12px'}}>Password is required !</div>}
 
-            <button style={styles.button} onClick={handleSignIn}>Sign In</button>
-        </div>
+
+            <button style={{...styles.button,
+                backgroundColor: email && password ? "#007bff" : "#999",
+                cursor: email && password ? "pointer" : "not-allowed",
+            }} type="submit" disabled={!email || !password}>Sign In</button>
+        </form>
     );
 };
 
@@ -51,7 +86,7 @@ const styles={
         backgroundColor:"#007bff",
         color:"white",
         border:"none",
-        curson:"pointer"
+        cursor:"pointer"
     },
 };
 
